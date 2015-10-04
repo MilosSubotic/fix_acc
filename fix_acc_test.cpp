4fadd5ba45a605177af255e93f7640ffc367080e
Milos Subotic <milos.subotic.sm@gmail.com>
2015-10-04 14:50:37 +0200

    Problems. Nice debugging.

--- fix_acc_test.cpp
+++ fix_acc_test.cpp
@@ -8,6 +8,7 @@
  */
 
 ///////////////////////////////////////////////////////////////////////////////
+
 #include "fix_acc_test.h"
 
 #include <cassert>
@@ -15,6 +16,8 @@
 #include <iomanip>
 
 ///////////////////////////////////////////////////////////////////////////////
+
+#define ENABLE_LOGGING 1
 
 #define BIGGEST_EXPONENT 254
 #define BIGGEST_MANTISA 0x7fffff
@@ -32,24 +35,25 @@ inline float fields_to_float(
 	return fu.f;
 }
 
+
 float fasp_2_float(fix_acc::fasp fa) {
 	return float(fa);
 }
 
-float float_2_fasp_and_back(float f) {
+float float_2_fasp_and_back(float f){
 	fix_acc::fasp fa(f);
 	//DEBUG_HEX(fa);
 	return float(fa);
 }
 
-void check_float_2_fasp_and_back(float f0) {
+void check_float_2_fasp_and_back(float f0){
 	float f1 = float_2_fasp_and_back(f0);
 	assert(f0 == f1);
 }
 
-template<typename AccType>
-AccType sum_vec(std::vector<float> to_sum) {
-	AccType acc = 0;
+template<typename T>
+T ordinary_sum(std::vector<T> to_sum) {
+	T acc = 0;
 	for(auto iter = to_sum.begin(); iter != to_sum.end(); iter++){
 		acc += *iter;
 	}
@@ -57,10 +61,11 @@ AccType sum_vec(std::vector<float> to_sum) {
 }
 
 // Implementation of Kahan
-template<typename T> T kahan_sum(std::vector<T> for_sum) {
+template<typename T> 
+T kahan_sum(std::vector<T> for_sum) {
 	T sum = 0.0;
 	T c = 0.0;
-	for(auto iter = for_sum.begin(); iter != for_sum.end(); iter++){
+	for(auto iter = for_sum.begin(); iter != for_sum.end(); iter++) {
 		T y = *iter - c;
 
 		T t = sum + y;
@@ -69,45 +74,79 @@ template<typename T> T kahan_sum(std::vector<T> for_sum) {
 	}
 	return sum;
 }
+ 
+float fix_acc_sum(std::vector<float> f) {
+	fix_acc::fasp acc = 0;
+	for(auto iter = f.begin(); iter != f.end(); iter++) {
+		acc += *iter;
+	}
+	
+	//DEBUG_HEX(acc);
+	return float(acc);
+}
+
 
 void test_problem() {
 	// One big many smalls problem.
 	float big = fields_to_float(0, 127, 0);
-	float small = fields_to_float(0, 127 - 24, 0);
-	float tiny = fields_to_float(0, 127 - 48, 0);
+	float small = fields_to_float(0, 127-24, 0);
+	float tiny = fields_to_float(0, 127-48, 0);
 
-	const int num_smalls_in_big = 1 << 24;
-	const int num_tinies_in_smalls = 1 << 24;
+    const int num_smalls_in_big = 1 << 24;
+    const int num_tinies_in_smalls = 1 << 24;
 
+	std::vector<float> many_smalls((1 << 24) + 1);
+	for(auto iter = many_smalls.begin(); iter != many_smalls.end(); iter++){
+		*iter = small;
+	}
+
+    auto k = small/tiny;
+	//DEBUG(k);
+
+	std::vector<float> many_tinies(k);
+	for(auto iter = many_tinies.begin(); iter != many_tinies.end(); iter++) {
+		*iter = tiny;
+	}
 
 	std::vector<float> smalls_and_big(num_smalls_in_big + 1, small);
-	smalls_and_big[smalls_and_big.size() - 1] = big;
+	smalls_and_big[smalls_and_big.size()-1] = big;
 
+	// Because small is less that big's epsilon adding doesn't work.
 	std::vector<float> big_and_smalls(1 + num_smalls_in_big, small);
 	big_and_smalls[0] = big;
 
-	std::vector<float> big_smalls_and_tinies(
-			1 + num_smalls_in_big/2 + num_tinies_in_smalls*num_smalls_in_big/2,
-			tiny);
-	big_smalls_and_tinies[0] = big;
-	for(int i = 1; i < 1 + num_smalls_in_big/2; i++){
-		big_smalls_and_tinies[i] = small;
+	// Aditional problem       
+	std::vector<float> big_and_smalls_and_tinies(1 + num_smalls_in_big + 2*num_tinies_in_smalls, tiny);
+	big_and_smalls_and_tinies[0] = big;
+	for(int i = 1; i < num_smalls_in_big + 1; i++) {
+		big_and_smalls_and_tinies[i] = small;
 	}
 
+#if ENABLE_LOGGING
+    DEBUG(ordinary_sum(smalls_and_big));
+	DEBUG(ordinary_sum(big_and_smalls));
+	DEBUG(ordinary_sum(big_and_smalls_and_tinies));
+	
+	DEBUG(kahan_sum(smalls_and_big));
+	DEBUG(kahan_sum(big_and_smalls));
+	DEBUG(kahan_sum(big_and_smalls_and_tinies));
+	
+	DEBUG(fix_acc_sum(smalls_and_big));
+	DEBUG(fix_acc_sum(big_and_smalls));
+	DEBUG(fix_acc_sum(big_and_smalls_and_tinies));	
+#endif
 
-	// Checking algorithms with problems.
-
-	assert(sum_vec<float>(smalls_and_big) == 2.0);
-	// Adding fails because small is less that big's epsilon.
-	assert(sum_vec<float>(big_and_smalls) == 1.0);
-	// This also doesn't work.
-	assert(sum_vec<float>(big_smalls_and_tinies) == 1.0);
+	assert(ordinary_sum(smalls_and_big) == 2.0);
+	assert(ordinary_sum(big_and_smalls) == 1.0);
+	assert(ordinary_sum(big_and_smalls_and_tinies) == 1.0);
 
 	assert(kahan_sum(smalls_and_big) == 2.0);
 	assert(kahan_sum(big_and_smalls) == 2.0);
-	// Kahan fails because tiny is less than small's epsilon
-	// and because that calculation of y in Kahan fails.
-	assert(kahan_sum(big_smalls_and_tinies) == 1.5);
+	assert(kahan_sum(big_and_smalls_and_tinies) == 2.0);
+
+	assert(fix_acc_sum(smalls_and_big) == 2.0);
+	assert(fix_acc_sum(big_and_smalls) == 2.0);
+	assert(fix_acc_sum(big_and_smalls_and_tinies) == 2.0 + 2*small);
 }
 
 void test_constructors_and_conversion() {
@@ -115,7 +154,7 @@ void test_constructors_and_conversion() {
 	using namespace std;
 	using namespace fix_acc;
 
-	// Highest bit in a[0].
+ 	// Highest bit in a[0].
 	float zero = fields_to_float(0, 0, 0);
 	check_float_2_fasp_and_back(zero);
 
@@ -134,6 +173,7 @@ void test_constructors_and_conversion() {
 	// Highest bit in a[1].
 	check_float_2_fasp_and_back(fields_to_float(0, 42, 0));
 
+
 	// Highest bit in a[4].
 	float biggest_normal = fields_to_float(
 			0,
@@ -141,7 +181,10 @@ void test_constructors_and_conversion() {
 			BIGGEST_MANTISA);
 	check_float_2_fasp_and_back(biggest_normal);
 
-	float infinity = fields_to_float(0, BIGGEST_EXPONENT + 1, 0);
+	float infinity = fields_to_float(
+			0,
+			BIGGEST_EXPONENT + 1,
+			0);
 	check_float_2_fasp_and_back(infinity);
 
 	for(uint32_t e = 0; e < BIGGEST_EXPONENT + 1; e++){
@@ -192,7 +235,7 @@ uint128_t shift_and_add(uint128_t acc, uint64_t mantisa, uint8_t shift) {
 	return acc;
 }
 
-void test1() {
+void test1(){
 	using namespace std;
 
 	uint128_t u128 = shift_and_add(uint128_t(1), 0xf000000000000000L, 3);
@@ -232,8 +275,8 @@ void test2() {
 	assert(z2 == 9);
 }
 
-void fix_acc_test() {
-	std::cout.precision(20);
+void fix_acc_test(){
+    std::cout.precision(20);
 	test_problem();
 	//test_constructors_and_conversion();
 	test_addition_assignment();
